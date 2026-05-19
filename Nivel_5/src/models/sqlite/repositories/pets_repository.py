14e08f606b6015049f3ed40d458054e225470426
jones-bass <jonesdev.tb@gmail.com>
2026-05-19 19:58:19 -0300

    Add testes

--- Nivel_5/src/models/sqlite/repositories/pets_repository.py
+++ Nivel_5/src/models/sqlite/repositories/pets_repository.py
@@ -1,10 +1,9 @@
 from typing import List
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.pets import PetsTable
-from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 
 
-class PetsRepository(PetsRepositoryInterface):
+class PetsRepository:
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
 
