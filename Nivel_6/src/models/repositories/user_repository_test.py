5e1884493bcf77ec652e318b4b2604f02e0e4c33
jones-bass <jonesdev.tb@gmail.com>
2026-05-30 08:41:25 -0300

    Add Repositories e testes

---
+++ Nivel_6/src/models/repositories/user_repository_test.py
@@ -0,0 +1,17 @@
+from .user_repository import UserRepository
+
+from src.models.settings.db_connection_handler import db_connection_handler
+
+def test_registry_user():
+    db_connection_handler. connect ()
+    conn = db_connection_handler.get_connection()
+    repo = UserRepository(conn)
+
+    username = "Jones"
+    password = "Souza"
+
+    repo.registry_user(username, password)
+
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)
