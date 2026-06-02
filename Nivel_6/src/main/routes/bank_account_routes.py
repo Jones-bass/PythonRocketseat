1b606bed722a317d5f69db0101c3892eb0c2b778
jones-bass <jonesdev.tb@gmail.com>
2026-06-01 21:19:53 -0300

    Create Register

---
+++ Nivel_6/src/main/routes/bank_account_routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify, request
+from src.views.http_types.http_request import HttpRequest
+
+from src.main.composer.user_register_composer import user_register_composer
+
+from src.errors.error_handler import handle_errors
+
+bank_routes_bp = Blueprint("bank_routes", __name__)
+
+@bank_routes_bp.route("/bank/registry", methods=["POST"])
+def registry_user():
+    try:
+        http_request = HttpRequest(body=request.json)
+        http_response = user_register_composer().handle(http_request)
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = handle_errors(exception)
+        return jsonify(http_response.body), http_response.status_code
+
+
