7d30f32ee1cfc60895f39248ca8729d8440201b3
jones-bass <jonesdev.tb@gmail.com>
2026-07-02 19:35:27 -0300

    Add Controllers + tests

---
+++ Nivel_10/src/controllers/user_register_test.py
@@ -0,0 +1,70 @@
+# pylint:disable = C1803
+import pytest
+from src.errors.types.http_bad_request_error import HttpBadRequestError
+from .user_register import UserRegister
+
+
+class UserRepositoryMock:
+    def __init__(self):
+        self.insert_users_att = {}
+
+    async def insert_users(self, user_data: dict):
+        self.insert_users_att["user_data"] = user_data
+
+
+@pytest.mark.asyncio
+async def test_register_user():
+    user_repository = UserRepositoryMock()
+    user_register = UserRegister(user_repository)
+
+    user_data = {
+        "user_name": "Maria Silva",
+        "age": 32,
+        "uf": "MG"
+    }
+
+    response = await user_register.register_user(user_data)
+    print()
+    print(response)
+
+    assert user_repository.insert_users_att["user_data"] == user_data
+
+    assert response["type"] == "USERS"
+    assert response["count"] == 1
+    assert response["attributes"] == user_data
+
+
+@pytest.mark.asyncio
+async def test_register_user_error_uf():
+    user_repository = UserRepositoryMock()
+    user_register = UserRegister(user_repository)
+
+    invalid_uf_user_data = {
+        "user_name": "Maria Silva",
+        "age": 32,
+        "uf": "ES"
+    }
+
+    with pytest.raises(HttpBadRequestError) as excinfo:
+        await user_register.register_user(invalid_uf_user_data)
+
+    assert str(excinfo.value) == "Estado invalido para cadastro"
+    assert user_repository.insert_users_att == {}
+
+
+@pytest.mark.asyncio
+async def test_register_user_error_age():
+    user_repository = UserRepositoryMock()
+    user_register = UserRegister(user_repository)
+
+    invalid_uf_user_data = {
+        "user_name": "Maria Silva",
+        "age": -32,
+        "uf": "ES"
+    }
+
+    with pytest.raises(HttpBadRequestError) as excinfo:
+        await user_register.register_user(invalid_uf_user_data)
+
+    assert str(excinfo.value) == "Idade invalida para cadastro"
+    assert user_repository.insert_users_att == {}
