77957934f6df7bf992912b3b41ef066b1e6341af
jones-bass <jonesdev.tb@gmail.com>
2026-07-04 17:05:10 -0300

    implment Views

---
+++ Nivel_10/src/views/user_register_view.py
@@ -0,0 +1,17 @@
+from src.controllers.interfaces.user_register import UserRegisterInterface
+from src.errors.error_handler import error_handler
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+
+
+class UserRegisterView:
+    def __init__(self, controller: UserRegisterInterface) -> None:
+        self.__controller = controller
+
+    async def handle_register_user(self, http_request: HttpRequest) -> HttpResponse:
+        try:
+            user_data = http_request.body
+            response = await self.__controller.register_user(user_data)
+            return HttpResponse(body=response, status_code=201)
+        except Exception as exception:
+            error_handler(exception)
