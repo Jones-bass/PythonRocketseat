37b62caaf78d113ac9d8e3cfde0b19da47295be5
jones-bass <jonesdev.tb@gmail.com>
2026-07-05 16:00:36 -0300

    Add erros + validators + composer e finalianzo projeto

--- Nivel_10/src/main/routes/users_routes.py
+++ Nivel_10/src/main/routes/users_routes.py
@@ -1,9 +1,33 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from src.main.composer.user_register_composer import user_register_composer
+from src.main.composer.user_finder_composer import user_finder_composer
+from src.validators.user_register_validator import UserInput
+from src.views.http_types.http_request import HttpRequest
+
 
 users_routes = APIRouter(tags=["Usuários"])
 
 
-@users_routes.get("/users")
-async def criar_usuario():
-    return JSONResponse(content={"message": "Rota de usuários funcionando!"})
+@users_routes.post("/users")
+async def criar_usuario(body: UserInput):
+    http_request = HttpRequest(body=dict(body))
+    user_register = user_register_composer()
+    http_response = await user_register.handle_register_user(http_request)
+    return JSONResponse(
+        content=http_response.body,
+        status_code=http_response.status_code
+    )
+
+
+@users_routes.get("/users/{user_name}")
+async def buscar_usuarios_por_nome(user_name: int):
+    http_request = HttpRequest(path_params={"user_name": user_name})
+    user_finder = user_finder_composer()
+
+    http_response = await user_finder.handle_find_user_by_name(http_request)
+    return JSONResponse(
+        content=http_response.body,
+        status_code=http_response.status_code
+    )
+    
